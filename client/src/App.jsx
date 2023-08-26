64649c4d98a135076bd9f49f35027904e6edc544
tech-teni <yakubganiyat96@gmail.com>
2023-08-26 18:08:41 +0100

    added frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <header>
+        <h1>StraitPay</h1>
+
+        <ul>
+          <li>Home</li>
+        </ul>
+      </header>
+    </section>
+  );
+};
+
+export default Home;
