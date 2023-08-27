ac534d3fc7a582bdea2f26ede793d3b2540ec7ed
tech-teni <yakubganiyat96@gmail.com>
2023-08-27 21:34:27 +0100

    chnaged the /task route to / in the server

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {});
 app.use("/", taskRoute);
 
 // Serve static assets in production
